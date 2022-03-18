636ad512a86c5e854e4d0e7b3ff2bb78f9043074
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-03-19 02:24:29 +0530

    Update checker.py

--- Hero/Decorators/checker.py
+++ Hero/Decorators/checker.py
@@ -6,22 +6,22 @@ def checker(mystic):
     async def wrapper(_, message):
         if message.sender_chat:
             return await message.reply_text(
-                "ʏᴏᴜ'ʀᴇ ᴀɴ __ᴀɴᴏɴʏᴍᴏᴜs ᴀᴅᴍɪɴ__ ɪɴ ᴛʜɪs ᴄʜᴀᴛ ɢʀᴏᴜᴘ...\nʀᴇᴠᴇʀᴛ ʙᴀᴄᴋ ᴛᴏ ᴜsᴇʀ ᴀᴄᴄᴏᴜɴᴛ ғʀᴏᴍ ᴀᴅᴍɪɴ ʀɪɢʜᴛs..."
+                "You're an __Anonymous Admin__ In This Chat Group...\nRevert Back to User account from Admin Rights..."
             )
         blacklisted_chats_list = await blacklisted_chats()
         if message.chat.id in blacklisted_chats_list:
             await message.reply_text(
-                f"**ʙʟᴀᴄᴋʟɪsᴛᴇᴅ ᴄʜᴀᴛ**\n\nʏᴏᴜʀ ᴄʜᴀᴛ ʜᴀs ʙᴇᴇɴ ʙʟᴀᴄᴋʟɪsᴛᴇᴅ ʙʏ sᴜᴅᴏ ᴜsᴇʀs ᴀsᴋ ᴀɴʏ __SUDO USER__ ᴛᴏ ᴡʜɪᴛᴇʟɪsᴛ...\nᴄʜᴇᴄᴋ sᴜᴅᴏ ᴜsᴇʀs ʟɪsᴛ [ғʀᴏᴍ ʜᴇʀᴇ](https://t.me/{BOT_USERNAME}?start=sudolist)"
+                f"**Blacklisted Chat**\n\nYour Chat has been Blacklisted by Sudo User Ask Any __SUDO USER__ To Whitelist...\nCheck Sudo Userlist [From Here](https://t.me/{BOT_USERNAME}?start=sudolist)"
             )
             return await app.leave_chat(message.chat.id)
         if await is_on_off(1):
             if int(message.chat.id) != int(LOG_GROUP_ID):
                 return await message.reply_text(
-                    f"ʙᴏᴛ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ sᴏʀʀʏ ғᴏʀ ᴛʜᴇ ɪɴᴄᴏɴᴠᴇɴɪᴇɴᴄᴇ"
+                    f"Bot Is Under Maintenance Sorry For The Inconvenience"
                 )
         if await is_gbanned_user(message.from_user.id):
             return await message.reply_text(
-                f"**ɢʙᴀɴɴᴇᴅ ᴜsᴇʀ**\n\nʏᴏᴜ'ʀᴇ ɢʙᴀɴɴᴇᴅ ғʀᴏᴍ ᴜsɪɴɢ ʙᴏᴛ ᴀsᴋ ᴀɴʏ __SUDO USER__ ᴛᴏ ᴜɴɢʙᴀɴ...\nᴄʜᴇᴄᴋ sᴜᴅᴏ ᴜsᴇʀs ʟɪsᴛ [ғʀᴏᴍ ʜᴇʀᴇ](https://t.me/{BOT_USERNAME}?start=sudolist)"
+                f"**Gbanned User**\n\nYou're Gbanned From using bot ask any __SUDO USER__ To Ungban...\nCheck Sudo User list [From Here](https://t.me/{BOT_USERNAME}?start=sudolist)"
             )
         return await mystic(_, message)
 
@@ -33,12 +33,12 @@ def checkerCB(mystic):
         blacklisted_chats_list = await blacklisted_chats()
         if CallbackQuery.message.chat.id in blacklisted_chats_list:
             return await CallbackQuery.answer(
-                "ʙʟᴀᴄᴋʟɪsᴛᴇᴅ ᴄʜᴀᴛ", show_alert=True
+                "Blacklisted Chat", show_alert=True
             )
         if await is_on_off(1):
             if int(CallbackQuery.message.chat.id) != int(LOG_GROUP_ID):
                 return await CallbackQuery.answer(
-                    "ʙᴏᴛ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ sᴏʀʀʏ ғᴏʀ ᴛʜᴇ ɪɴᴄᴏɴᴠᴇɴɪᴇɴᴄᴇ...",
+                    "Bot Is Under Maintainec Sorry For The Convenience...",
                     show_alert=True,
                 )
         if await is_gbanned_user(CallbackQuery.from_user.id):
