3037bf669acf17b6358cf5127716678e32ea5faf
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-03-19 14:42:15 +0530

    Update Blacklist.py

--- Hero/Plugins/Blacklist.py
+++ Hero/Plugins/Blacklist.py
@@ -4,65 +4,65 @@ from pyrogram.types import Message
 from Hero import SUDOERS, app
 from Hero.Database import blacklist_chat, blacklisted_chats, whitelist_chat
 
-__MODULE__ = "ʙʟᴀᴄᴋʟɪsᴛ"
+__MODULE__ = "Blacklist"
 __HELP__ = """
 
 
-`/blacklistedchat`
-- ᴄʜᴇᴄᴋ ʙʟᴀᴄᴋʟɪsᴛᴇᴅ ᴄʜᴀᴛs ᴏғ ʙᴏᴛ.
+/blacklistedchat 
+- Check Blacklisted Chats of Bot.
 
 
-**ɴᴏᴛᴇ:**
-ᴏɴʟʏ ғᴏʀ sᴜᴅᴏ ᴜsᴇʀs.
+**Note:**
+Only for Sudo Users.
 
 
-`/blacklistchat` [ᴄʜᴀᴛ_ɪᴅ] 
-- ʙʟᴀᴄᴋʟɪsᴛ ᴀɴʏ ᴄʜᴀᴛ ғʀᴏᴍ ᴜsɪɴɢ ᴍᴜsɪᴄ ʙᴏᴛ
+/blacklistchat [CHAT_ID] 
+- Blacklist any chat from using Music Bot
 
 
-`/whitelistchat` [ᴄʜᴀᴛ_ɪᴅ] 
-- ᴡʜɪᴛᴇʟɪsᴛ ᴀɴʏ ʙʟᴀᴄᴋʟɪsᴛᴇᴅ ᴄʜᴀᴛ ғʀᴏᴍ ᴜsɪɴɢ ᴍᴜsɪᴄ ʙᴏᴛ
+/whitelistchat [CHAT_ID] 
+- Whitelist any blacklisted chat from using Music Bot
 
 """
 
 
-@app.on_message(filters.command(["black", "blacklistchat"]) & filters.user(SUDOERS))
+@app.on_message(filters.command("blacklistchat") & filters.user(SUDOERS))
 async def blacklist_chat_func(_, message: Message):
     if len(message.command) != 2:
         return await message.reply_text(
-            "**ᴜsᴀɢᴇ:**\n/blacklistchat [CHAT_ID]"
+            "**Usage:**\n/blacklistchat [CHAT_ID]"
         )
     chat_id = int(message.text.strip().split()[1])
     if chat_id in await blacklisted_chats():
-        return await message.reply_text("ᴄʜᴀᴛ ɪs ᴀʟʀᴇᴀᴅʏ ʙʟᴀᴄᴋʟɪsᴛᴇᴅ...")
+        return await message.reply_text("Chat is already blacklisted.")
     blacklisted = await blacklist_chat(chat_id)
     if blacklisted:
         return await message.reply_text(
-            "ᴄʜᴀᴛ ʜᴀs ʙᴇᴇɴ sᴜᴄᴄᴇssғᴜʟʟʏ ʙʟᴀᴄᴋʟɪsᴛᴇᴅ"
+            "Chat has been successfully blacklisted"
         )
-    await message.reply_text("sᴏᴍᴇᴛʜɪɴɢ ᴡʀᴏɴɢ ʜᴀᴘᴘᴇɴᴇᴅ, ᴄʜᴇᴄᴋ ʟᴏɢs...")
+    await message.reply_text("Something wrong happened, check logs.")
 
 
-@app.on_message(filters.command(["white", "whitelistchat"]) & filters.user(SUDOERS))
+@app.on_message(filters.command("whitelistchat") & filters.user(SUDOERS))
 async def whitelist_chat_func(_, message: Message):
     if len(message.command) != 2:
         return await message.reply_text(
-            "**ᴜsᴀɢᴇ:**\n/whitelistchat [CHAT_ID]"
+            "**Usage:**\n/whitelistchat [CHAT_ID]"
         )
     chat_id = int(message.text.strip().split()[1])
     if chat_id not in await blacklisted_chats():
-        return await message.reply_text("ᴄʜᴀᴛ ɪs ᴀʟʀᴇᴀᴅʏ ᴡʜɪᴛᴇʟɪsᴛᴇᴅ...")
+        return await message.reply_text("Chat is already whitelisted.")
     whitelisted = await whitelist_chat(chat_id)
     if whitelisted:
         return await message.reply_text(
-            "ᴄʜᴀᴛ ʜᴀs ʙᴇᴇɴ sᴜᴄᴄᴇssғᴜʟʟʏ ᴡʜɪᴛᴇ ʟɪsᴛᴇᴅ..."
+            "Chat has been successfully whitelisted"
         )
-    await message.reply_text("sᴏᴍᴇᴛʜɪɴɢ ᴡʀᴏɴɢ ʜᴀᴘᴘᴇɴᴇᴅ, ᴄʜᴇᴄᴋ ʟᴏɢs...")
+    await message.reply_text("Something wrong happened, check logs.")
 
 
 @app.on_message(filters.command("blacklistedchat"))
 async def blacklisted_chats_func(_, message: Message):
-    text = "**ʙʟᴀᴄᴋʟɪsᴛᴇᴅ ᴄʜᴀᴛs:**\n\n"
+    text = "**Blacklisted Chats:**\n\n"
     j = 0
     for count, chat_id in enumerate(await blacklisted_chats(), 1):
         try:
@@ -72,6 +72,6 @@ async def blacklisted_chats_func(_, message: Message):
         j = 1
         text += f"**{count}. {title}** [`{chat_id}`]\n"
     if j == 0:
-        await message.reply_text("ɴᴏ ʙʟᴀᴄᴋʟɪsᴛᴇᴅ ᴄʜᴀᴛs")
+        await message.reply_text("No Blacklisted Chats")
     else:
         await message.reply_text(text)
